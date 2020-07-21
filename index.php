49b76a446867d811e957f3c539a8b38b6c6fa3e1
ABIKWA Alexandre <Abikalex8@gmail.com>
2020-07-21 19:52:16 +0200

    fomction avec formulaire pour effectuer des calcul

--- index.php
+++ index.php
@@ -62,14 +62,94 @@
             <aside class="answer"><!--  structure conditionnelle-->
                 <?php
                     echo "<span class='question'>structure conditionnelle</span>";
-                    $table = array(10,21,32,43,55);
-                    echo "c'est untableau de  <b>5</b> valeurs dont <br>". 
-                    "le premier est  <span id='nom'><font color='trail'>".$table[0]."</font></span><br>".
-                    "le deuxieme est  <span id='nom'><font color='trail'>".$table[1]."</font></span><br>".
-                    "le troisieme est  <span id='nom'><font color='trail'>".$table[2]."</font></span><br>".
-                    "le quatrieme est  <span id='nom'><font color='trail'>".$table[3]."</font></span><br>".
-                    "le cinquieme est  <span id='nom'><font color='trail'>".$table[4]."</font></span><br>";
+                    $table = array(27,21,32,13,55, 88);
+                    for($i = 0; $i<5; $i++)
+                    {
+                        $j = $i+1;
+                        $k = $i + 2;
+                        if($table[$i] < $table[$j])
+                            echo "la Valeur numero ".$j ." <span id='nom'><font color='trail'>".$table[$i]."</font></span> est <span id='nom'><font color='blue'>inferieur</font></span> a la valeur numero ".$k." <span id='nom'><font color='trail'>".$table[$j]."</font></span><br>";
+                        else if($table[$i] > $table[$j])
+                            echo "la Valeur numero ".$j ." <span id='nom'><font color='trail'>".$table[$i]."</font></span> est <span id='nom'><font color='blue'>superieur</font></span> a la valeur numero ".$k." <span id='nom'><font color='trail'>".$table[$j]."</font></span><br>";
+                        else
+                            echo "la Valeur numero ".$j ." <span id='nom'><font color='trail'>".$table[$i]."</font></span> est <span id='nom'><font color='blue'>egale</font></span> a la valeur numero ".$k." <span id='nom'><font color='trail'>".$table[$j]."</font></span><br>";
+                    }
                 ?>
+            </aside>
+            <aside class="answer"><!--  fonction-->
+                <?php
+                    echo "<span class='question'>fonction</span>";
+                    function calculatrice($val1, $val2, $type)
+                    {
+                        $resultat;
+                        if(intval($val1) > 0)
+                        {
+                            $val1 = intval($val1);
+                            $val2 = intval($val2);
+                            if($type == "+")
+                                $resultat = $val1 + $val2;
+                            elseif($type == "*")
+                                $resultat = $val1 + $val2;
+                            elseif($type == "/")
+                                $resultat = $val1 / $val2;
+                            elseif($type == "-")
+                                $resultat = $val1 - $val2;
+                            else
+                                $resultat = "ce type d'operation n'est pas disponible";
+                        }
+                        else
+                            $resultat = "la premiere valeur doit etre au moins superieur a zero";
+                        return $resultat;
+                    }
+                    if(isset($_POST['calculer']))
+                    {
+                        if(isset($_POST['val1']) and !empty($_POST['val1']))
+                        {
+                            if(isset($_POST['val2']) and !empty($_POST['val2']))
+                            {
+                                if(isset($_POST['add']))
+                                {
+                                    $nbr1 = htmlspecialchars($_POST['val1']);
+                                    $nbr2 = htmlspecialchars($_POST['val2']);
+                                    $calc = $_POST['add'];
+                                   $resultat =  calculatrice($nbr1, $nbr2, $calc);
+                                }
+                                else
+                                    $resultat = "choisir le type de calcul";
+                            }else
+                                $resultat = "saisir la deuxieme valeur aussi";
+                        }else
+                            $resultat = "saisir la prmiere valeur aussi";
+                    }
+                    if(isset($nbr1))
+                    {
+                        echo " <span id='nom'><font color='trail'>".$nbr1."</font></span> ".$calc."  <span id='nom'><font color='trail'>".$nbr2."</font></span> = <span id='nom'><font color='blue'>".$resultat."</font></span><br>";
+                   
+                    }
+                    else
+                        echo "<span id='nom'><font color='red'>".$resultat."</font></span>";
+                ?>
+                <form action="" method="POST">
+                    <table>
+                        <tr>
+                            <td><input type="number" name="val1" /></td>
+                        </tr>
+                        <tr>
+                            <td><input type="number" name="val2" /></td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input type="radio" name="add"  value="+" id="addition" /> <label for="addition">Addition</label>
+                                <input type="radio" name="add"  value="-" id="sustr" /> <label for="sustr">Soustraction</label>
+                                <input type="radio" name="add"  value="*" id="mult" /> <label for="mult">Multiplication</label>
+                                <input type="radio" name="add"  value="/" id="sou" /> <label for="sou">Division</label>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><input type="submit" name="calculer" value = "Calculer" /></td>
+                        </tr>
+                    </table>
+                </form>
             </aside>
         </article>
     </div>
